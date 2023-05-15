e869f104fdddb62b6a99600599b6d74b44eb0de9
Zarna Pathak <pathakzarna0128@gmail.com>
2023-05-15 16:23:41 +0530

    Zarna/Home/Contact_About_FAQ_Confirmation_Error Pages Added
    Added Contact, About, FAQ, Confirmation, Error Pages and Loaded Static for these pages.

--- Fashion_Blossom/Home/urls.py
+++ Fashion_Blossom/Home/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('shop',views.shop,name='shop'),
+    path('contact',views.contact,name='contact'),
+    path('about',views.about,name='about'),
+    path('confirmation',views.confirmation,name='confirmation'),
+    path('faq',views.faq,name='faq'),
+    path('error',views.error,name='error'),
 ]
--- Fashion_Blossom/Home/views.py
+++ Fashion_Blossom/Home/views.py
@@ -6,3 +6,18 @@ def home(request):
 
 def shop(request):
     return render(request,'shop.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def error(request):
+    return render(request,'404.html')
+
+def faq(request):
+    return render(request,'faq.html')
+
+def confirmation(request):
+    return render(request,'confirmation.html')
